860ec2d630f89bed572cb9fbf3c9ca7f9fe1679c
Pengcheng Lv <lvpengcheng6300@gmail.com>
2019-04-10 06:12:01 +0800

    Update objectPool.go

--- memory/objectPool.go
+++ memory/objectPool.go
@@ -1,3 +1,30 @@
+/* usage:
+func main() {
+	p := memory.NewObjectPool(func() interface{} {
+		return nil
+	})
+
+	p.Put("1", 1)
+	p.Put("2", 2)
+	p.Put("3", 3)
+	p.Put("4", 4)
+
+	log.Infof("%v", p.Get("1"))
+	log.Infof("%v", p.Get("2"))
+	log.Infof("%v", p.Get("3"))
+	log.Infof("%v", p.Get("4"))
+	log.Infof("%v", p.Get("5"))
+	log.Infof("%v", p.Get("6"))
+
+	------  output  --------
+	2019/04/10 06:05:07 [INFO] main.go:19:  1
+	2019/04/10 06:05:07 [INFO] main.go:20:  2
+	2019/04/10 06:05:07 [INFO] main.go:21:  3
+	2019/04/10 06:05:07 [INFO] main.go:22:  4
+	2019/04/10 06:05:07 [INFO] main.go:23:  <nil>
+	2019/04/10 06:05:07 [INFO] main.go:24:  <nil>
+}
+*/
 package memory
 
 import (
