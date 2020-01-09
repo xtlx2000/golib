a52573c47644725b132c1f3b71345952c0cfb98c
Pengcheng Lv <lvpengcheng6300@gmail.com>
2020-01-09 11:43:44 +0800

    add rotatelog

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/xtlx2000/golib/rotateLog"
 )
-
-type inter interface {
-	Start()
-}
-
-type interStr struct {
-}
-
-func newInter() inter {
-	return &interStr{}
-}
-
-func (i *interStr) Start() {
-
-}
 
 func main() {
 
-	i := newInter()
-	fmt.Printf("%v", i)
+	log.SteupLog("name.log", 5*1024, 5)
+	log.Errorf("wocao %v", nil)
+	log.Errorf("wocao1 %v", "string")
+	log.Errorf("wocao2 %v", 5)
+	log.Errorf("wocao3 %v", 6.6)
+
+	for i := 0; i < 10000; i++ {
+		log.Errorf("wocao3 %v", 6.6)
+	}
 }
---
+++ rotateLog/log.go
@@ -0,0 +1,33 @@
+package log
+
+import (
+	"github.com/xtlx2000/golib/rotateLog/mlog"
+)
+
+func SteupLog(filename string, maxBytes, number int) {
+	mlog.StartEx(mlog.LevelInfo, filename, maxBytes, number)
+}
+
+func main() {
+
+	mlog.Info("Hello World !")
+
+	ipsum := "ipsum"
+	mlog.Warning("Lorem %s", ipsum)
+}
+
+func Infof(format string, a ...interface{}) {
+	mlog.Info(format, a...)
+}
+
+func Warningf(format string, a ...interface{}) {
+	mlog.Warning(format, a...)
+}
+
+func Errorf(format string, a ...interface{}) {
+	mlog.Errorf(format, a...)
+}
+
+func Fatalf(format string, a ...interface{}) {
+	mlog.Fatalf(format, a...)
+}
