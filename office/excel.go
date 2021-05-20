557a087a1284675ac2f5f16eaaf97fbc7c57b37d
Pengcheng Lv <lvpengcheng6300@gmail.com>
2021-05-21 02:40:33 +0800

    add office excel operation

--- office/excel.go
+++ office/excel.go
@@ -3,24 +3,28 @@ package main
 import "github.com/360EntSecGroup-Skylar/excelize"
 
 func main() {
-	f, err := excelize.OpenFile("Book1.xlsx")
+	f, err := excelize.OpenFile("test.xlsx")
 	if err != nil {
 		println(err.Error())
 		return
 	}
 	// 获取工作表中指定单元格的值
-	cell, err := f.GetCellValue("Sheet1", "B2")
-	if err != nil {
-		println(err.Error())
-		return
-	}
+	cell := f.GetCellValue("BigThings", "D2")
 	println(cell)
 	// 获取 Sheet1 上所有单元格
-	rows, err := f.GetRows("Sheet1")
+	rows := f.GetRows("BigThings")
 	for _, row := range rows {
 		for _, colCell := range row {
 			print(colCell, "\t")
 		}
 		println()
+	}
+
+	f.SetCellValue("BigThings", "H1", "T7 Hello world.")
+	f.SetCellValue("BigThings", "H2", 666)
+	f.UpdateLinkedValue()
+	err = f.Save()
+	if err != nil {
+		print(err)
 	}
 }
