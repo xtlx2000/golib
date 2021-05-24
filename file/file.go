e204d2ca06355b59cf76fcaab3e11098a5f95785
Pengcheng Lv <lvpengcheng6300@gmail.com>
2021-05-24 17:36:00 +0800

    Update file.go

--- file/file.go
+++ file/file.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"io"
 	"io/ioutil"
 )
 
@@ -11,4 +12,29 @@ func WriteFile(filename, text string) error {
 func ReadFile(filename string) (string, error) {
 	content, err := ioutil.ReadFile(filename)
 	return string(content), err
+}
+
+func CopyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
