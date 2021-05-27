da2b84db6fd87dfb602cb945753255ac93ceaa45
Pengcheng Lv <lvpengcheng6300@gmail.com>
2021-05-27 14:19:15 +0800

    add smtp

---
+++ smtp/smtp.go
@@ -0,0 +1,27 @@
+package smtp
+
+import (
+	"net/smtp"
+	"strings"
+)
+
+func SendToMail(sendUserName, to, subject, body string) error {
+	user := "80181339@qq.com"
+	password := "fmfnaolzwhdhcace"
+	host := "smtp.qq.com"
+	mailtype := "html"
+
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	var content_type string
+	if mailtype == "html" {
+		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+
+	msg := []byte("To: " + to + "\r\nFrom: " + sendUserName + "<" + user + ">" + "\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+	send_to := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, send_to, msg)
+	return err
+}
